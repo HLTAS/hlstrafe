ada5fbb5339c16f7c1c540de8b0ab29d8d265a13
Matherunner <randolphchong@gmail.com>
2015-01-16 14:21:57 +0800

    Implemented radians version of anglemod

--- src/util.hpp
+++ src/util.hpp
@@ -9,6 +9,8 @@ namespace HLStrafe
 #endif
 	const double M_RAD2DEG = 180 / M_PI;
 	const double M_DEG2RAD = M_PI / 180;
+	const double M_U_RAD = M_PI / 32768;
+	const double M_INVU_RAD = 32768 / M_PI;
 
 	template<typename T, std::size_t size = 3>
 	inline bool IsZero(const T vec[])
@@ -27,5 +29,10 @@ namespace HLStrafe
 		for (std::size_t i = 0; i < size; ++i)
 			squared += vec[i] * vec[i];
 		return std::sqrt(squared);
+	}
+
+	inline double AngleModRad(double a)
+	{
+		return M_U_RAD * ((int)(a * M_INVU_RAD) & 0xffff);
 	}
 }
